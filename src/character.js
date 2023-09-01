ad41bdc39f54ca9520f3d4909dc74246837d6d87
Ekaterina Markeeva <lesekaterina25@gmail.com>
2023-09-01 18:48:24 +0400

    instances of the class

--- src/character.js
+++ src/character.js
@@ -4,10 +4,10 @@ export default class Character {
   constructor(name, type) {
     
     if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
-      throw new Error("Имя должно быть не менее 2 и не более 10 символов");
+      throw new Error ("Имя должно быть не менее 2 и не более 10 символов");
     }
     if (!typesOfClasses[type]) {
-      throw new Error("Тип не найден");
+      throw new Error ("Тип не найден");
     }
 
     this.name = name;
